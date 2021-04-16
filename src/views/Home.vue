35f86f7dbbab7f0e60eb322a8f99a15c053c453f
mauricio <mauricio2017rubiale@gmail.com>
2021-04-16 17:01:04 -0300

    VUE-Blog Default

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Blog msg=""></Blog>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Blog from '@/components/Blog.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Blog
   }
 }
 </script>
